af8dd17f3d79fec3fb133ed522957b702a44e9a3
Serhii Zakharov <szakharo@redhat.com>
2020-02-12 10:45:37 +0100

    refactored functions SendSomething to use Send

--- responses/responses.go
+++ responses/responses.go
@@ -66,48 +66,35 @@ func Send(statusCode int, w http.ResponseWriter, data interface{}) {
 
 // SendResponse returns JSON response
 func SendResponse(w http.ResponseWriter, data map[string]interface{}) {
-	setDefaultContentType(w) // doesn't work if WriteHeader has been called on w first
-	json.NewEncoder(w).Encode(data)
+	Send(http.StatusOK, w, data)
 }
 
 // SendCreated returns response with status Created
 func SendCreated(w http.ResponseWriter, data map[string]interface{}) {
-	setDefaultContentType(w)
-	w.WriteHeader(http.StatusCreated)
-	SendResponse(w, data)
+	Send(http.StatusCreated, w, data)
 }
 
 // SendAccepted returns response with status Accepted
 func SendAccepted(w http.ResponseWriter, data map[string]interface{}) {
-	setDefaultContentType(w)
-	w.WriteHeader(http.StatusAccepted)
-	SendResponse(w, data)
+	Send(http.StatusAccepted, w, data)
 }
 
 // SendError returns error response
 func SendError(w http.ResponseWriter, err string) {
-	setDefaultContentType(w)
-	w.WriteHeader(http.StatusBadRequest)
-	SendResponse(w, BuildResponse(err))
+	Send(http.StatusBadRequest, w, err)
 }
 
 // SendForbidden returns response with status Forbidden
 func SendForbidden(w http.ResponseWriter, err string) {
-	setDefaultContentType(w)
-	w.WriteHeader(http.StatusForbidden)
-	SendResponse(w, BuildResponse(err))
+	Send(http.StatusForbidden, w, err)
 }
 
 // SendInternalServerError returns response with status Internal Server Error
 func SendInternalServerError(w http.ResponseWriter, err string) {
-	setDefaultContentType(w)
-	w.WriteHeader(http.StatusInternalServerError)
-	SendResponse(w, BuildResponse(err))
+	Send(http.StatusInternalServerError, w, err)
 }
 
 // SendUnauthorized returns error response for unauthorized access
 func SendUnauthorized(w http.ResponseWriter, data map[string]interface{}) {
-	setDefaultContentType(w)
-	w.WriteHeader(http.StatusUnauthorized)
-	SendResponse(w, data)
+	Send(http.StatusUnauthorized, w, data)
 }
