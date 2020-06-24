1473662eebbf67d83b4d1b0849c9217b3e9c7e83
Serhii Zakharov <szakharo@redhat.com>
2020-06-24 22:46:54 +0200

    fixed-some-bugs

--- tests/helpers/json.go
+++ tests/helpers/json.go
@@ -4,7 +4,26 @@ import "encoding/json"
 
 // ToJSONString converts anything to JSON or panics if it's not possible
 func ToJSONString(obj interface{}) string {
-	jsonBytes, err := json.MarshalIndent(obj, "", "\t")
+	return toJSONString(obj, false)
+}
+
+// ToJSONPrettyString converts anything to indented JSON or panics if it's not possible
+func ToJSONPrettyString(obj interface{}) string {
+	return toJSONString(obj, true)
+}
+
+// toJSONString converts anything to JSON or panics if it's not possible
+// isOutputPretty makes output indented
+func toJSONString(obj interface{}, isOutputPretty bool) string {
+	var (
+		jsonBytes []byte
+		err       error
+	)
+	if isOutputPretty {
+		jsonBytes, err = json.MarshalIndent(obj, "", "\t")
+	} else {
+		jsonBytes, err = json.Marshal(obj)
+	}
 	if err != nil {
 		panic(err)
 	}
