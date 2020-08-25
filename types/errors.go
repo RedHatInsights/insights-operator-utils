1723acda44c2cb54fcb270fe1c4d8a49a04d3f24
Serhii Zakharov <szakharo@redhat.com>
2020-08-25 13:12:28 +0200

    added-some-more-common-functions

--- types/errors.go
+++ types/errors.go
@@ -73,7 +73,7 @@ func HandleServerError(writer http.ResponseWriter, err error) {
 	var respErr error
 
 	switch err := err.(type) {
-	case *RouterMissingParamError, *RouterParsingError, *json.SyntaxError, *NoBodyError:
+	case *RouterMissingParamError, *RouterParsingError, *json.SyntaxError, *NoBodyError, *ValidationError:
 		respErr = responses.SendBadRequest(writer, err.Error())
 	case *json.UnmarshalTypeError:
 		respErr = responses.SendBadRequest(writer, "bad type in json data")
