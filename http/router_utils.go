d53013d2752a4613c5574a2a7a4e23d6aa4e6c08
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-11-05 13:00:31 +0100

    Handle empty body

--- http/router_utils.go
+++ http/router_utils.go
@@ -277,6 +277,13 @@ func ReadClusterListFromPath(writer http.ResponseWriter, request *http.Request) 
 func ReadClusterListFromBody(writer http.ResponseWriter, request *http.Request) ([]string, bool) {
 	var clusterList types.ClusterListInRequest
 
+	// check if there's any body provided in the request sent by client
+	if request.ContentLength <= 0 {
+		err := &NoBodyError{}
+		types.handleServerError(writer, err)
+		return []string{}, false
+	}
+
 	// try to read cluster list from request parameter
 	err := json.NewDecoder(request.Body).Decode(&clusterList)
 	if err != nil {
