ab39311eba9bd8cb05cbd8db8cdff952162dfc4d
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-05-18 14:32:05 +0200

    Link to generated documentation for http/router_utils package

--- http/router_utils_test.go
+++ http/router_utils_test.go
@@ -14,6 +14,9 @@
 
 package httputils_test
 
+// Documentation in literate-programming-style is available at:
+// https://redhatinsights.github.io/insights-operator-utils/packages/http/router_utils_test.html
+
 import (
 	"context"
 	"fmt"
