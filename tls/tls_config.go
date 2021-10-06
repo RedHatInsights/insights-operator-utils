69f76caec9c352ffcc6094c26070f60316847b4e
Juan Diaz Suarez <jdiazsua@redhat.com>
2021-10-06 09:04:06 +0200

    mock the tests so we can reach a 100% coverage

--- tls/tls_config.go
+++ tls/tls_config.go
@@ -8,9 +8,29 @@ import (
 	"path/filepath"
 )
 
+type tlsConfigGetter interface {
+	newCertPoolGetter() *x509.CertPool
+	appendCertsFromPEMGetter(caCertPool *x509.CertPool, pemCerts []byte) (ok bool)
+}
+
+type tlsConfigX509Getter struct{}
+
 // NewTLSConfig create a TLS configuration from a certificate path. This can be
 // used with Sarama for example.
 func NewTLSConfig(certPath string) (*tls.Config, error) {
+	tcg := new(tlsConfigX509Getter)
+	return newTLSConfig(tcg, certPath)
+}
+
+func (t tlsConfigX509Getter) newCertPoolGetter() *x509.CertPool {
+	return x509.NewCertPool()
+}
+
+func (t tlsConfigX509Getter) appendCertsFromPEMGetter(caCertPool *x509.CertPool, pemCerts []byte) (ok bool) {
+	return caCertPool.AppendCertsFromPEM(pemCerts)
+}
+
+func newTLSConfig(t tlsConfigGetter, certPath string) (*tls.Config, error) {
 	if certPath == "" {
 		return nil, fmt.Errorf("no cert path provided. Skip")
 	}
@@ -24,12 +44,12 @@ func NewTLSConfig(certPath string) (*tls.Config, error) {
 	if err != nil {
 		return nil, err
 	}
-	caCertPool := x509.NewCertPool()
-	if caCert == nil {
+	caCertPool := t.newCertPoolGetter()
+	if caCertPool == nil {
 		return nil, fmt.Errorf("pointer to new CertPool is nil")
 	}
 
-	ok := caCertPool.AppendCertsFromPEM(caCert)
+	ok := t.appendCertsFromPEMGetter(caCertPool, caCert)
 	if !ok {
 		return nil, fmt.Errorf("error appending the specified certificate")
 	}
