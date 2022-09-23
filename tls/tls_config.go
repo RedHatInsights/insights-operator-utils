a30393cfdd147dbd9fcb1d0376b1619756af1efa
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-09-23 11:26:25 +0200

    Better style

--- tls/tls_config.go
+++ tls/tls_config.go
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// Helper function to create TLS configurations
+// Package tlsutil contains helper function to create TLS configurations
 package tlsutil
 
 import (
