b97aaeff34adbfc8724b9c6395d711b14981424a
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-09-23 11:36:58 +0200

    Colorized govet.sh

--- govet.sh
+++ govet.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-# Copyright 2020 Red Hat, Inc
+
+# Copyright 2020, 2021 Red Hat, Inc
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -13,8 +14,24 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+BLUE=$(tput setaf 4)
+RED_BG=$(tput setab 1)
+GREEN_BG=$(tput setab 2)
+NC=$(tput sgr0) # No Color
 
 cd "$(dirname "$0")" || exit
 
+echo -e "${BLUE}go vet check${NC}"
+
 # shellcheck disable=SC2046
 go vet $(go list ./...)
+
+# shellcheck disable=SC2181
+if [[ $? -ne 0 ]]
+then
+    echo -e "${RED_BG}[FAIL]${NC} go vet tool finds issues in source code"
+    exit 1
+else
+    echo -e "${GREEN_BG}[OK]${NC} All source codes are correct according to go vet"
+    exit 0
+fi
