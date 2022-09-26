43896cc6f124e1e7b829c8a37bc687c135a0a4a2
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-09-26 09:22:37 +0200

    Repository-level documentation

---
+++ utils.go
@@ -0,0 +1,20 @@
+/*
+Copyright © 2019, 2020, 2021, 2022 Red Hat, Inc.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+// Utility packages (written in Go) that are shared between different Insights
+// Operator, Insights Results Aggregator, and CCX Notification Service
+// repositories.
+package main
