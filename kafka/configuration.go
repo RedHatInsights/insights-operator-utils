cb75f7de5cc17a2e093f10529b8ffbf7ce0b6852
Papa Bakary Camara <pcamara@redhat.com>
2024-01-15 15:07:04 +0100

    Correct TOML mapstructure for BrokerConfiguration addresses field

--- kafka/configuration.go
+++ kafka/configuration.go
@@ -31,7 +31,7 @@ import (
 
 // BrokerConfiguration represents configuration of a single-instance Kafka broker
 type BrokerConfiguration struct {
-	Addresses        []string      `mapstructure:"address" toml:"address"`
+	Addresses        []string      `mapstructure:"addresses" toml:"addresses"`
 	SecurityProtocol string        `mapstructure:"security_protocol" toml:"security_protocol"`
 	CertPath         string        `mapstructure:"cert_path" toml:"cert_path"`
 	SaslMechanism    string        `mapstructure:"sasl_mechanism" toml:"sasl_mechanism"`
