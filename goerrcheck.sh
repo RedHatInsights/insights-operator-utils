fd873d1c2eaac747f816d19d9dc56168eea73fa5
Jakub007d <dr.kubko@gmail.com>
2025-03-13 08:23:31 +0100

    Updating version of go to 1.22

--- goerrcheck.sh
+++ goerrcheck.sh
@@ -25,7 +25,7 @@ echo -e "${BLUE}Finding all unchecked errors${NC}"
 if ! [ -x "$(command -v errcheck)" ]
 then
     echo -e "${BLUE}Installing errcheck ${NC}"
-    GO111MODULE=on go install github.com/kisielk/errcheck@latest
+    go install github.com/kisielk/errcheck@latest
 fi
 
 
